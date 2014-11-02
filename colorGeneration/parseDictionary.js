7c4ce9b427b8083842ba6bc9678f525dc78c1a94
jupegarnica <jupegarnica@gmail.com>
2014-11-03 00:49:12 +0100

    spanish dictonary added

--- colorGeneration/parseDictionary.js
+++ colorGeneration/parseDictionary.js
@@ -1,15 +1,36 @@
 var fs = require ('fs');
-fs.readFile('dictionary.json', function (err, data) {
-  if (err) throw err;
-  var dictionary_6_3 = mapDictionary( JSON.parse(data));
-  saveWords(dictionary_6_3);
-});
-function mapDictionary (dic) {
+function extractFromJson(file){
+
+  fs.readFile(file, function (err, data) {
+    if (err) throw err;
+    var output = mapDictionaryJson( JSON.parse(data));
+    saveWords(output,file + '.parsed.json');
+  });
+}
+function extractFromTxt(file){
+  fs.readFile(file,'utf8', function (err, data) {
+    if (err) throw err;
+    var output = mapDictionaryText( data);
+    saveWords(output,file + '.parsed.json');
+  });
+}
+function mapDictionaryText (dic) {
+
+  var words = dic.split('\n');
+  var words_6_3 = [];
+  var max = words.length;
+  words.forEach(function(word){
+    if (word.length == 6 || word.length == 3 ){
+      words_6_3.push(word);
+    }
+  });
+  return words_6_3;
+}
+function mapDictionaryJson (dic) {
 
   var words = Object.keys(dic);
   var words_6_3 = [];
   var max = words.length;
-  console.log(max);
   words.forEach(function(word){
     if (word.length == 6 || word.length == 3 ){
       words_6_3.push(word);
@@ -18,9 +39,9 @@ function mapDictionary (dic) {
   return words_6_3;
 }
 
-function saveWords(words) {
+function saveWords(words,file) {
   var dictionary_6_3 = toObject(words);
-  fs.writeFile('dictionary_6_3.json', JSON.stringify(dictionary_6_3) , function (err) {
+  fs.writeFile(file, JSON.stringify(dictionary_6_3) , function (err) {
   if (err) throw err;
   console.log('It\'s saved!');
 });
@@ -32,3 +53,4 @@ function toObject(arr) {
     if (arr[i] !== undefined) rv[i] = arr[i];
   return rv;
 }
+extractFromTxt('/Users/jpg/Desktop/bada55/colorGeneration/words.txt');
---
+++ colorGeneration/words2colors.js
@@ -0,0 +1,83 @@
+words2colors('/Users/jpg/Desktop/bada55/colorGeneration/spanishdict.txt.parsed.json');
+
+function words2colors(file){
+  var fs = require('fs');
+  var words = [];
+  var colors = {}; //  word : color
+  fs.readFile(file, function(err, data) {
+    if (err) throw err;
+    words = toArray(JSON.parse(data));
+    var color = "";
+    for (var i = 0; i < words.length; i++) {
+
+      color = word2color(words[i]);
+
+      if (color) {
+        colors[words[i].toUpperCase()] = color;
+      }
+
+    }
+    fs.writeFile(file+'.colors.json', JSON.stringify(colors), function(err) {
+      if (err) throw err;
+      console.log('It\'s saved!');
+    });
+  });
+}
+
+function toArray(obj) {
+  var i = 0,
+  array = [];
+  while (obj[i]) {
+    array.push(obj[i++]);
+  }
+  return array;
+}
+
+var rosetaHex2Letter = {};
+rosetaHex2Letter['1'] = ['L', 'I'];
+rosetaHex2Letter['2'] = ['Q', 'Z', 'R'];
+rosetaHex2Letter['3'] = ['E', 'B'];
+rosetaHex2Letter['4'] = ['A', 'P'];
+rosetaHex2Letter['5'] = ['S'];
+rosetaHex2Letter['6'] = ['G'];
+rosetaHex2Letter['7'] = ['T'];
+rosetaHex2Letter['8'] = ['B', 'O'];
+rosetaHex2Letter['9'] = ['G', 'Q'];
+rosetaHex2Letter['0'] = ['O'];
+rosetaHex2Letter['A'] = ['A'];
+rosetaHex2Letter['B'] = ['B'];
+rosetaHex2Letter['C'] = ['C'];
+rosetaHex2Letter['D'] = ['D'];
+rosetaHex2Letter['E'] = ['E'];
+rosetaHex2Letter['F'] = ['F'];
+
+var rosetaLetter2Hex = {};
+rosetaLetter2Hex['A'] = ['4', 'A'];
+rosetaLetter2Hex['B'] = ['8', 'B'];
+rosetaLetter2Hex['C'] = ['C'];
+rosetaLetter2Hex['D'] = ['D'];
+rosetaLetter2Hex['E'] = ['E'];
+rosetaLetter2Hex['F'] = ['F'];
+rosetaLetter2Hex['G'] = ['6'];
+rosetaLetter2Hex['I'] = ['1'];
+rosetaLetter2Hex['L'] = ['1'];
+rosetaLetter2Hex['O'] = ['0'];
+rosetaLetter2Hex['P'] = ['4'];
+rosetaLetter2Hex['Q'] = ['9'];
+rosetaLetter2Hex['R'] = ['2'];
+rosetaLetter2Hex['S'] = ['5'];
+rosetaLetter2Hex['T'] = ['7'];
+rosetaLetter2Hex['Z'] = ['2'];
+
+function word2color(word) {
+  var color = "#";
+  var splited = word.toUpperCase().split('');
+  for (var i = 0; i < splited.length; i++) {
+    if (rosetaLetter2Hex[splited[i]]) {
+      color += rosetaLetter2Hex[splited[i]][0];
+    } else {
+      return false;
+    }
+  }
+  return color;
+}
