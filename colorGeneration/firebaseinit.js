18a15fe332ad52fc40086d275503db3ef749b614
jupegarnica <jupegarnica@gmail.com>
2015-03-06 11:16:21 +0000

    webmastertools verification

--- colorGeneration/firebaseinit.js
+++ colorGeneration/firebaseinit.js
@@ -5,13 +5,15 @@
  * Time: 03:02 PM
  */
 var Firebase = require("firebase");
-var myFirebaseRef = new Firebase("https://fiery-heat-8886.firebaseio.com/");
+var myFirebaseRef = new Firebase("https://fiery-heat-8886.firebaseio.com/colors");
 var colors;
 var fs = require('fs');
 fs.readFile('all.array.json', function(err, data) {
     if(err) throw err;
     colors = JSON.parse(data);
+    ff
 //     myFirebaseRef.set({
-//         'colors': colors
+//         'olddata': colors,
+//         'colors':'hola mundo'
 //     });
 });
